f28028b5e4ccb1978df4c31ca15b4108c7680485
N Eliseo S Carranza <neli92santscz@gmail.com>
2018-07-16 19:47:28 -0400

    bar progress

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,17 +1,68 @@
-import * as d3 from 'd3';
+ import * as d3 from 'd3';
 /*
 d3.csv("assets/data/data.csv").then(function(data){
     console.log(data)
     generate(data.columns)
-});*/
+});
+let data_json;
+d3.json('https://data.cityofnewyork.us/resource/waf7-5gvc.json').then(function(data){
 
-d3.json('https://data.cityofnewyork.us/resource/kuz6-hiwt.json').then(function(data){
-    generate(data);
-    console.log(data)
+    data.forEach(function(d){
+        return d;
+
+    })
 })
+.then((d)=>{
+    data_json+=d
+})*/
 
-//var dataset=[10,20,30,40,50];
 
+
+
+/*
+d3.csv("/data/cities.csv", function(data) {
+  data.forEach(function(d) {
+    d.population = +d.population;
+    d["land area"] = +d["land area"];
+  });
+  console.log(data[0]);
+});
+
+*/
+
+var dataset=[];
+var chart_width = 800;
+var chart_height = 400;
+
+
+for (var i=0;i<10;i++){
+     var num = Math.floor(d3.randomUniform(1,50)());
+     dataset.push(num);
+}
+console.log(dataset.map(function(d,i){
+    return `${i+1} ${d}`;
+}))
+
+
+var svg = d3.select('#chart')
+    .append('svg')
+    .attr('height',chart_height)
+    .attr('width',chart_width);
+
+
+// bind dATA AND create bars
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x',function(d,i){
+        return i * (chart_width/dataset.length);
+    })
+    .attr('y',0)
+    .attr('width',25)
+    .attr('height',100);
+
+/*
 function generate(dataset){
     var el = d3.select('body')
         .selectAll('p')
@@ -24,4 +75,4 @@ function generate(dataset){
 
 
     console.log(dataset)
-}
+}*/

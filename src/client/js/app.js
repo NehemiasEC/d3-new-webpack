51d356bf7f75cc52a4f31551f4b6a0b2cce799dc
N Eliseo S Carranza <neli92santscz@gmail.com>
2018-07-19 23:40:37 -0400

    slices

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -99,6 +99,13 @@ svg2.selectAll('text')
         return d[1]
     })
 
+var slices = [100,200,300,400,500,600]
+var scale = d3.scaleLinear()
+    .domain([d3.min(slices),d3.max(slices)])
+    .range([0,100])
+
+console.log(scale(600));
+
 
 
 
